8ad8fb199c4e7927a3d5403d4788f46419cf9fe0
ngphuc2808 <ngphuc.2808@gmail.com>
2022-04-26 10:24:50 +0700

    Update slider for list product homepage

--- assets/js/frontend/plugin.js
+++ assets/js/frontend/plugin.js
@@ -10,6 +10,33 @@ $(document).ready(function(){
     nextArrow: `<button class="slick-next"><i class="fa-solid fa-angle-right"></i></button>`,
     prevArrow: `<button class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>`
   });
+
+  $('.list-product').slick({
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    infinite: false,
+    arrows: true,
+    nextArrow: `<button class="slick-next-1"><i class="fa-solid fa-angle-right"></i></button>`,
+    prevArrow: `<button class="slick-prev-1"><i class="fa-solid fa-angle-left"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 1319,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 739,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+    ]
+  });
+
   $('.favorite-rating').rateYo({
     rating: 0,
     spacing: '3px',
