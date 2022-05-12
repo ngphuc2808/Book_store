b68f3659f0af891289350c7139ee1fb5def50f07
ngphuc2808 <ngphuc.2808@gmail.com>
2022-05-12 17:57:36 +0700

    Update form admin

--- app.js
+++ app.js
@@ -11,8 +11,24 @@ app.get('/HomePage', (req, res) => {
     res.render('HomePage.ejs');
 })
 
+app.get('/Admin', (req, res) => {
+  res.render('Admin.ejs');
+})
+
 app.get('/User', (req, res) => {
   res.render('FormUserProfile.ejs');
+})
+
+app.get('/Product', (req, res) => {
+  res.render('FormDetailProduct.ejs');
+})
+
+app.get('/Category', (req, res) => {
+  res.render('FormProductCategory.ejs');
+})
+
+app.get('/Writter', (req, res) => {
+  res.render('FormWritterProduct.ejs');
 })
 
 app.listen(port, () => {
